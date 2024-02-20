a73a155ea19a481854fad99923dc98896717659b
Helio Albano de Oliveira <helioalb@gmail.com>
2024-02-20 17:55:43 -0300

    Add import/export examples

---
+++ exercicios/src/components/modulo/funcionais.jsx
@@ -0,0 +1,11 @@
+export function Comp1() {
+  return <h2>Comp #01</h2>
+}
+
+export const Comp2 = function() {
+  return <h2>Comp #02</h2>
+}
+
+export default function Comp3() {
+  return <h2>Comp #03</h2>
+}
---
+++ exercicios/src/pages/modulo/teste.jsx
@@ -0,0 +1,11 @@
+import Comp3, { Comp1, Comp2 } from "../../components/modulo/funcionais";
+
+export default function test() {
+  return (
+    <div>
+      <Comp1 />
+      <Comp2 />
+      <Comp3 />
+    </div>
+  )
+}
